6b2c656d583b209f4192b56b505a671ff3f5ed52
HO <love47024702@naver.com>
2024-09-01 14:06:16 +0900

    7.1.1 회원 저장

--- src/main/java/chapter6to12/next/web/Controller/CreateUserController.java
+++ src/main/java/chapter6to12/next/web/Controller/CreateUserController.java
@@ -1,6 +1,7 @@
 package chapter6to12.next.web.Controller;
 
 import chapter6to12.core.db.DataBase;
+import chapter6to12.next.dao.UserDao;
 import chapter6to12.next.model.User;
 import chapter6to12.next.mvc.AbstractController;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 public class CreateUserController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
@@ -17,8 +19,15 @@ public class CreateUserController extends AbstractController {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
         log.debug("user : {}", user);
+
+        UserDao userDao = new UserDao();
+        try {
+            userDao.insert(user);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
         DataBase.addUser(user);
-        return "redirect:/user/list";
+        return "redirect:/";
     }
 
     @Override
---
+++ src/main/java/chapter6to12/next/web/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package chapter6to12.next.web.Controller;
+
+import chapter6to12.next.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeController extends AbstractController {
+
+    @Override
+    protected String doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return "index.jsp";
+    }
+}
