faab5b59cbd39cd99af36327066b1fbfa48e841e
HO <love47024702@naver.com>
2024-09-11 22:01:52 +0900

    9.10. 질문 답변 삭제

--- src/main/java/chapter6to12/next/web/Controller/qna/DeleteAnswerController.java
+++ src/main/java/chapter6to12/next/web/Controller/qna/DeleteAnswerController.java
@@ -1,6 +1,8 @@
 package chapter6to12.next.web.Controller.qna;
 
 import chapter6to12.next.dao.AnswerDao;
+import chapter6to12.next.dao.QuestionDao;
+import chapter6to12.next.model.Answer;
 import chapter6to12.next.model.Result;
 import chapter6to12.next.mvc.AbstractController;
 import chapter6to12.next.mvc.ModelAndView;
@@ -11,11 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 public class DeleteAnswerController extends AbstractController {
     @Override
     protected ModelAndView doPost(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Long answerId = Long.parseLong(request.getParameter("answerId"));
         AnswerDao answerDao = new AnswerDao();
+        QuestionDao questionDao = new QuestionDao();
 
-        answerDao.delete(answerId);
+        Answer answer = answerDao.findById(Long.parseLong(request.getParameter("answerId")));
 
+        questionDao.decreaseCount(answer.getQuestionId());
+        answerDao.delete(answer.getAnswerId());
         return getJsonView().addObject("result", Result.ok());
     }
 }
