f2bbf5b1b508325f510e958f303fbd9a21a1bcac
HO <love47024702@naver.com>
2024-09-13 22:07:18 +0900

    10. 환경 설정

---
+++ src/main/java/chapter6to12/core/nmvc/HandlerExecution.java
@@ -0,0 +1,13 @@
+package chapter6to12.core.nmvc;
+
+
+import chapter6to12.next.mvc.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HandlerExecution {
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
