86cb04752a3f6229a18b19903a2aa67524599b87
HO <love47024702@naver.com>
2024-09-21 15:44:40 +0900

    10.1.2.3 @MyTest 어노테이션으로 설정된 메소드 실행

--- src/test/java/chapter6to12/core/ref/Junit4TestRunner.java
+++ src/test/java/chapter6to12/core/ref/Junit4TestRunner.java
@@ -2,10 +2,21 @@ package chapter6to12.core.ref;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
+        final List<Method> methodList = Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .toList();
 
+        for (Method method : methodList) {
+            method.invoke(instance);
+        }
     }
 }
